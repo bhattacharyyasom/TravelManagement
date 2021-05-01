3787e7d131699fe4488f2cdc4883a0e52fe064e2
Som Shankar Bhattacharyya <bhattacharyyasom@gmail.com>
2021-05-01 18:38:58 +0530

    Som | Added dummy test

--- tw.travel.desk/src/main/java/twtraveldesk/StubMain.java
+++ tw.travel.desk/src/main/java/twtraveldesk/StubMain.java
@@ -5,7 +5,6 @@ import tw.travel.flights.Flights;
 public class StubMain {
     public static void main(String[] args) {
         System.out.println("Starting Booking");
-
         Flights fls = new Flights();
         System.out.println(fls.createFlightBooking());
     }
---
+++ tw.travel.flights/src/test/java/tw/travel/flights/FlightsTest.java
@@ -0,0 +1,12 @@
+package tw.travel.flights;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightsTest {
+    @Test
+    void createFlightBooking() {
+        assertTrue(true);
+    }
+}
